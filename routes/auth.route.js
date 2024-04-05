2de2a49cf0403abba419b45c6c4904aaf474675c
Rewan Adel <rewanadel1266@gmail.com>
2024-04-05 05:27:49 +0200

    Project Structure

--- routes/auth.route.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const {initialRegister} = require('../controllers/auth.controller');
-
-router.post('/register/initial/:method', initialRegister);
-
-module.exports = router;
