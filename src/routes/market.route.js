d821d60a387218edb0bb8e73c78b6391d89f7459
@Rewan-Adel <rewanadel1266@gmail.com>
2024-11-05 01:20:49 +0200

    Route

--- src/routes/market.route.js
+++ src/routes/market.route.js
@@ -3,16 +3,19 @@ const {
     createProduct,
     getAllProducts,
     updateProduct,
-    getOneProduct
+    getOneProduct,
+    deleteProduct
 } = require('../controllers/market.controller');
 const {uploadMultiple} = require('../utils/multer');
-const {protect,restrictTo, checkVerification} = require('../middlewares/auth.token'); 
+const {protect, checkVerification} = require('../middlewares/auth.token'); 
 
 router.use(protect);
 router.use(checkVerification);
+
 router.post("/add", uploadMultiple, createProduct);
 router.get("/get-all", getAllProducts);
 router.get("/get/:id", getOneProduct);
 router.patch("/update/:id", uploadMultiple, updateProduct);
+router.delete("/delete/:id", deleteProduct);
 
 module.exports = router;
